6d2af2e1283f29f37c296b56a828f0287b98c281
noko206 <61347127+noko206@users.noreply.github.com>
2023-07-01 12:59:24 +0900

    fix: is_prime

--- is_prime.cpp
+++ is_prime.cpp
@@ -1,6 +1,7 @@
 template <class T> bool is_prime(T n) {
+    if (n == 0 || n == 1) return false;
     for (T i = 2; i * i <= n; ++i) {
-        if (n % i == 0) return true;
+        if (n % i == 0) return false;
     }
-    return false;
+    return true;
 }
