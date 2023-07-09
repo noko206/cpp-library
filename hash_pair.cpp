d3d41e5edfd824f4a8d4d09750cbaf2ca9dbc210
noko206 <61347127+noko206@users.noreply.github.com>
2023-07-10 02:04:02 +0900

    add: hash_pair

---
+++ hash_pair.cpp
@@ -0,0 +1,16 @@
+// unordered_mapのキーをpair型にしたいとき
+struct Hash_Pair {
+    static size_t m_hash_pair_random;
+    template <class T1, class T2>
+    size_t operator()(const pair<T1, T2> &p) const {
+        auto hash1 = hash<T1>{}(p.first);
+        auto hash2 = hash<T2>{}(p.second);
+        size_t seed = 0;
+        seed ^= hash1 + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+        seed ^= hash2 + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+        seed ^= m_hash_pair_random + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+        return seed;
+    }
+};
+
+size_t Hash_Pair::m_hash_pair_random = (size_t)random_device()();
