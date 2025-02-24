f42aff574277f2e23953d2e397caade8ed607e76
noko206 <61347127+noko206@users.noreply.github.com>
2025-02-24 06:36:36 +0000

    fix: run_length_encodeの配列外参照を修正

--- run_length_encode.cpp
+++ run_length_encode.cpp
@@ -2,7 +2,7 @@ vector<pair<char, int>> run_length_encode(string s) {
     vector<pair<char, int>> ans;
     int i = 0, j = 0, n = s.length();
     while (i < n) {
-        while (s[i] == s[j] && j < n) {
+        while (j < n && s[i] == s[j]) {
             ++j;
         }
         ans.push_back({s[i], j - i});
