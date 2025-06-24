3647424d5894de7bf859fc6af4254245dbd7a0ea
noko206 <61347127+noko206@users.noreply.github.com>
2025-06-24 13:43:47 +0000

    fix: matrix_pow

--- matrix_pow.cpp
+++ matrix_pow.cpp
@@ -1,10 +1,10 @@
 template <class T>
-vector<vector<T>> matrix_pow(const vector<vector<T>> &a, long long n) {
-    vector<vector<T>> res(a.size(), vector<ll>(a.size()));
+vector<vector<T>> matrix_pow(vector<vector<T>> a, long long n) {
+    vector res(a.size(), vector<T>(a.size()));
     for (int i = 0; i < a.size(); i++) res[i][i] = 1;
     while (n > 0) {
-        if (n & 1) res = mat_mul(a, res);
-        a = mat_mul(a, a);
+        if (n & 1) res = matrix_mul(a, res);
+        a = matrix_mul(a, a);
         n >>= 1;
     }
     return res;
