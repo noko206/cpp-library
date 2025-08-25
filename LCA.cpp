9b07b5607e76608999b93be9d68479470ae64746
noko206 <61347127+noko206@users.noreply.github.com>
2025-08-25 14:51:20 +0000

    add: LCA (Lowest Common Ancestor)

---
+++ LCA.cpp
@@ -0,0 +1,53 @@
+// 最長共通祖先 (Lowest Common Ancestor)
+struct LCA {
+   public:
+    LCA(const vector<vector<int>> &G, int root = 0) { init(G, root); }
+
+    int query(int u, int v) {
+        if (dist[u] < dist[v]) swap(u, v);
+        int K = parent.size();
+        for (int k = 0; k < K; k++) {
+            if ((dist[u] - dist[v]) >> k & 1) {
+                u = parent[k][u];
+            }
+        }
+        if (u == v) return u;
+        for (int k = K - 1; k >= 0; k--) {
+            if (parent[k][u] != parent[k][v]) {
+                u = parent[k][u];
+                v = parent[k][v];
+            }
+        }
+        return parent[0][u];
+    }
+
+   private:
+    vector<vector<int>> parent;
+    vector<int> dist;
+
+    void init(const vector<vector<int>> &G, int root = 0) {
+        int V = G.size();
+        int K = 1;
+        while ((1 << K) < V) K++;
+        parent.assign(K, vector<int>(V, -1));
+        dist.assign(V, -1);
+        dfs(G, root, -1, 0);
+        for (int k = 0; k + 1 < K; k++) {
+            for (int v = 0; v < V; v++) {
+                if (parent[k][v] < 0) {
+                    parent[k + 1][v] = -1;
+                } else {
+                    parent[k + 1][v] = parent[k][parent[k][v]];
+                }
+            }
+        }
+    }
+
+    void dfs(const vector<vector<int>> &G, int v, int p, int d) {
+        parent[0][v] = p;
+        dist[v] = d;
+        for (auto e : G[v]) {
+            if (e != p) dfs(G, e, v, d + 1);
+        }
+    }
+};
